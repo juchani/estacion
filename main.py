9b962713df6f89dc6fd482878aa3ea0a72ecfd7c
juchani <victorfrancojuchani@gmail.com>
2024-08-04 14:45:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import micropython
 import network
 from wifi import Red
 Red("Familia Juchani","8884992sc")
-ver=35
+ver=37
 led=machine.Pin(12,machine.Pin.OUT)
 rl_1=machine.Pin(14,machine.Pin.OUT)
 
